2c2e5b08f210ba051483816165f1731456fbe77c
Gonzalo Galdós <gogaldos@gmail.com>
2023-11-22 17:35:53 +0100

    add data cleanse

--- src/PreprocessDataset.py
+++ src/PreprocessDataset.py
@@ -4,7 +4,16 @@ import os
 
 def split_records(dataFrame:pd.DataFrame) -> pd.DataFrame:
     outDataFrame = dataFrame
-    return outDataFrame 
+    for col in dataFrame.columns:
+        if col.startswith("P"):
+            dataFrame = dataFrame.applymap(lambda x: x.strip('()') if isinstance(x, str) else x)
+            splited = dataFrame[col].str.split(",")
+            splited = splited.apply(pd.Series)
+
+            outDataFrame[col+"X"] = splited[0]
+            outDataFrame[col+"Y"] = splited[1]
+            del outDataFrame[col]
+    return outDataFrame
 
 def fuse_dataframes(path) -> pd.DataFrame:
     outDf = None
@@ -12,14 +21,19 @@ def fuse_dataframes(path) -> pd.DataFrame:
         for name in files:
             if name.endswith(".csv"):
                 if outDf is None:
-                    outDf = pd.read_csv(os.path.join(root, name))
+                    outDf = pd.read_csv(os.path.join(root, name),index_col=0)
+                    print(outDf)
                 else:
                     print(os.path.join(root, name))
                     np = os.path.join(root, name)
-                    tmpDf = pd.read_csv(np)
+                    tmpDf = pd.read_csv(np,index_col=0)
                     outDf = pd.concat([outDf,tmpDf],ignore_index=True)
     return outDf
 
+
+
 if __name__ == "__main__":
     df = fuse_dataframes("./HandContourRecognition/")
-    print(df)
+    #print(df)
+    splited = split_records(df)
+    print(splited)
